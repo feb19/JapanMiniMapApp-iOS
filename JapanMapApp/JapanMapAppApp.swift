8eab393a9eee28d550671142aa58d8e931bd6469
feb19 <nt@feb19.jp>
2021-07-20 17:14:41 +0900

    Initial Commit

---
+++ JapanMapApp/JapanMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanMapAppApp.swift
+//  JapanMapApp
+//
+//  Created by Nobuhiro Takahashi on 2021/07/20.
+//
+
+import SwiftUI
+
+@main
+struct JapanMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
